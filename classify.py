d27c81511e768a0ca9af4fcaa9f0cde0ee87f3ae
miltend <gglonshakov@gmail.com>
2024-02-05 11:19:29 +0100

    test

--- classify.py
+++ classify.py
@@ -1,15 +1,7 @@
 #USAGE: python3 classify.py <query_file>
 import sys
-import string
 import numpy as np
 from math import sqrt
-from bs4 import BeautifulSoup
-
-
-def contain_punctuation(text):
-    punctuation = [char for char in string.punctuation + ' ']
-    result = any(char in text for char in punctuation)
-    return result
 
 
 def cosine_similarity(v1, v2):
@@ -78,53 +70,5 @@ for q in queries:
     qvec = mk_vector(vocab,ngrams)
     for cat,vec in vectors.items():
         cosines[cat] = cosine_similarity(vec,qvec)
-    top_category = sorted(cosines, key=cosines.get, reverse=True)[0]
-    print('top category: ', top_category)
-    with open(f"{top_category}/linear.txt") as f:
-        soup = BeautifulSoup(f, 'html.parser')
-    texts = []
-    for doc in soup.find_all("doc"):
-        texts.append(str(doc.contents[0]).strip().lower())
-    ns = sys.argv[2:]
-
-    dfs = {}
-
-
-    vector_list = []
-    for text in texts:
-        one_doc_tfs = {}
-        sum_freq = 0
-        for n in ns:
-            n = int(n)
-            for i in range(len(text) - n + 1):
-                ngram = text[i:i+n]
-                if not contain_punctuation(ngram):
-                    sum_freq += 1
-                    if ngram in one_doc_tfs:
-                        one_doc_tfs[ngram] += 1
-                    else:
-                        one_doc_tfs[ngram] = 1
-        for ngram in one_doc_tfs.keys():
-            if ngram in dfs:
-                dfs[ngram] += 1
-            else:
-                dfs[ngram] = 1
-        one_doc_tfs = normalise_tfs(one_doc_tfs, sum_freq)
-        one_doc_vector = np.zeros(len(vocab))
-        for ngram, tf in one_doc_tfs.items():
-            if ngram in vocab:
-                position = vocab.index(ngram)
-                one_doc_vector[position] = tf
-        vector_list.append(one_doc_vector)
-        
-    all_docs_matrix = np.vstack(vector_list)
-    cosines_docs = {}
-    for i in range(all_docs_matrix.shape[0]):
-        cosines_docs[i] = cosine_similarity(all_docs_matrix[i], qvec)
-    print('top 10 documents: ')
-    count = 0
-    for doc in sorted(cosines_docs, key=cosines_docs.get, reverse=True):
-        print(texts[doc], cosines_docs[doc], '\n\n')
-        count += 1
-        if count == 10:
-            break
+    for cat in sorted(cosines, key=cosines.get, reverse=True):
+        print(cat,cosines[cat])
